ed502a8ffd86869be9de99d616b198435f59a7b4
Henrik Finsberg <henrikfinsberg@hotmail.com>
2024-03-17 20:18:27 +0100

    Actually DG0 is second order

--- tests/test_monodomain_solver.py
+++ tests/test_monodomain_solver.py
@@ -81,6 +81,7 @@ def test_monodomain_splitting_analytic(odespace):
     v_error = dolfin.errornorm(v_exact, pde.state, "L2", degree_rise=2)
     # dolfin.File("v_exact.pvd") << dolfin.interpolate(v_exact, pde.V)
     # dolfin.File("v.pvd") << pde.state
+    print(v_error)
     assert v_error < 0.002
 
 
@@ -159,10 +160,7 @@ def test_monodomain_splitting_spatial_convergence(odespace):
 
     rates = [np.log(e1 / e2) / np.log(2) for e1, e2 in zip(errors[:-1], errors[1:])]
     cvg_rate = sum(rates) / len(rates)
-    if degree > int(ode_degree):  # DG_0 (ODE) -> CG_1 (PDE)
-        assert np.isclose(cvg_rate, int(ode_degree) + 1, rtol=0.1)
-    else:
-        assert np.isclose(cvg_rate, degree + 1, rtol=0.1)
+    assert np.isclose(cvg_rate, degree + 1, rtol=0.1)
 
 
 def test_monodomain_splitting_temporal_convergence():
