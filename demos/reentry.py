97165e96883cfcde0d7f4ba487d00858e44c0d98
Henrik Finsberg <henrikfinsberg@hotmail.com>
2024-07-02 10:12:31 +0200

    Update reetry demo to find the correct timing of the s2 stimulus

--- demos/reentry.py
+++ demos/reentry.py
@@ -17,11 +17,10 @@ results_folder = Path("results-reentry")
 save_every_ms = 1.0
 transverse = False
 # Increase this to make the simulation longer
-end_time = 10.0
+end_time = 800.0
 dt = 0.05
 save_freq = round(save_every_ms / dt)
-overwrite = False
-stim_amp = 5000.0
+overwrite = True
 mesh_unit = "cm"
 dx = 0.4 * beat.units.ureg("mm").to(mesh_unit).magnitude
 L = 5.0 * beat.units.ureg("cm").to(mesh_unit).magnitude
@@ -36,7 +35,7 @@ V = dolfin.FunctionSpace(data.mesh, "CG", 1)
 
 # For this simulation we will use a model from {cite}`courtemanche1998ionic`. The model is taken form https://models.physiomeproject.org/workspace/courtemanche_ramirez_nattel_1998
 #
-
+#
 
 print("Running model")
 
@@ -53,49 +52,105 @@ if not model_path.is_file():
     )
     model_path.write_text(code)
 
+# +
 import courtemanche_ramirez_nattel_1998
 
 model = courtemanche_ramirez_nattel_1998.__dict__
+# -
 
 
-# Membrane capacitance
+# Specify the membrane capacitance and the surface to volume ratio
 
 C_m = 1.0 * beat.units.ureg("uF/cm**2")
+chi = 1000.0 * beat.units.ureg("cm**-1")
 
 # Here we also reduce the conductance for the sodium channel to make sure that the speed of the traveling wave is reduced
 
 fun = model["forward_generalized_rush_larsen"]
 y = model["init_state_values"]()
 time = dolfin.Constant(0.0)
-parameters = model["init_parameter_values"](stim_amplitude=0.0, g_Na=2.5)
+parameters = model["init_parameter_values"](stim_amplitude=0.0)
 
-# The duration of the stimulation is set to 5 ms and the second stimulation is done 400 ms after the initial stimulation. This timing is important to get spiral wave appearing
+# The duration of the stimulation is set to 5 ms
 
 duration = 5.0
-delay = 400.0
-# We specify this stimulation protocol as an expressions
 
-# +
-expr = dolfin.Expression(
+# We don't know when the S2 stimulation should be triggered, and we will find this by monitoring the voltage in the S2 domain. For now let us just create a placeholder for the start of the S2 stimulation and just set the value to a large number
+
+s2_start = dolfin.Constant(9999999.0)
+
+# We specify this stimulation protocol as an expressions. We will first stimulate the left boundary and then after a delay we will stimulate the lower left quadrant. In this case we can very easily define this in a single expression as
+
+expr_single_s1s2 = dolfin.Expression(
     "(t < duration && x[0] < 0.05) ? 20.0 : "
     "((t > delay && t < delay+duration) && (x[0] < L / 2 && x[1] < L / 2)) ? 20.0 : 0.0",
     t=time,
     L=L,
-    delay=delay,
+    delay=s2_start,
     duration=duration,
     degree=0,
 )
 
-I_s = beat.base_model.Stimulus(
-    dz=dolfin.dx, expr=expr  # StimulationProtocol(time, L=L),
+# and then define the stimulus as
+
+I_s_s1s2 = beat.base_model.Stimulus(
+    dz=dolfin.dx, expr=expr_single_s1s2
+)
+
+# However, sometimes you might have a mesh function defining the stimulus domains, in which case it can be hard to do this using a single expression. Therefore we will show an approach where we have two different expressions and two different currents representing the two stimuli.
+#
+# First we define the S1 stimulation
+
+# +
+s1s2_markers = dolfin.MeshFunction("size_t", data.mesh, 2)
+s1s2_markers.set_all(0)
+
+s1_domain = dolfin.CompiledSubDomain("x[0] < 0.05")
+s1_marker = 1
+s1_domain.mark(s1s2_markers, s1_marker)
+
+I_s_s1 = beat.stimulation.define_stimulus(
+    chi=chi,
+    mesh=data.mesh,
+    mesh_unit=mesh_unit,
+    time=time,
+    start=0.0,
+    duration=duration,
+    amplitude=20_000,
+    subdomain_data=s1s2_markers,
+    marker=s1_marker
+)
+
+s2_domain = dolfin.CompiledSubDomain("x[0] > 0.05 && x[0] < L / 2 && x[1] < L / 2", L=L)
+s2_marker = 2
+s2_domain.mark(s1s2_markers, s2_marker)
+
+
+I_s_s2 = beat.stimulation.define_stimulus(
+    chi=chi,
+    mesh=data.mesh,
+    mesh_unit=mesh_unit,
+    time=time,
+    start=s2_start,
+    duration=duration,
+    amplitude=20_000,
+    subdomain_data=s1s2_markers,
+    marker=s2_marker
 )
 # -
 
+# Let us also compute the volume (or area) of the S2 domain which we will use later
+
+vol_s2_domain = dolfin.assemble(dolfin.Constant(1) * I_s_s2.dz)
+
+# Finally let us collect the two simulation currents into a list
+
+I_s = [I_s_s1, I_s_s2]
+
 # Next we set up the models
 
+# +
 V_ode = dolfin.FunctionSpace(data.mesh, "Lagrange", 1)
-parameters_ode = np.zeros((len(parameters), V_ode.dim()))
-parameters_ode.T[:] = parameters
 
 M = beat.conductivities.define_conductivity_tensor(
     f0=data.f0,
@@ -107,7 +162,7 @@ ode = beat.odesolver.DolfinODESolver(
     v_pde=pde.state,
     fun=fun,
     init_states=y,
-    parameters=parameters_ode,
+    parameters=parameters,
     num_states=len(y),
     v_index=model["state_index"]("V"),
 )
@@ -116,24 +171,10 @@ solver = beat.MonodomainSplittingSolver(pde=pde, ode=ode)
 fname = (results_folder / "V.xdmf").as_posix()
 beat.postprocess.delete_old_file(fname)
 
-# and we also pick 12 uniformly distributed points on the mesh and keep track of the voltage in these points
 
-points = np.array(
-    [
-        [0.1, 0.1],
-        [0.1, 0.5],
-        [0.1, 0.9],
-        [0.5, 0.1],
-        [0.5, 0.5],
-        [0.5, 0.9],
-        [0.9, 0.1],
-        [0.9, 0.5],
-        [0.9, 0.9],
-    ]
-)
-V_values: list[list[float]] = [[] for _ in points]
-times = []
+# -
 
+# And create a function to be called when saving
 
 def save(t):
     v = solver.pde.state.vector().get_local()
@@ -149,47 +190,38 @@ def save(t):
             True,
         )
 
-    times.append(t)
-    for i, p in enumerate(points):
-        V_values[i].append(solver.pde.state(p))
-    fig, ax = plt.subplots()
-    for p in points:
-        ax.plot(times, V_values[i], label=f"{p}")
-    ax.set_xlabel("Time [ms]")
-    ax.set_ylabel("V [mV]")
-    ax.legend(title="Point")
-    fig.savefig(results_folder / "V.png")
-    plt.close()
-    print("Saved to ", results_folder / "V.png")
-    np.save(
-        results_folder / "V.npy",
-        {"V": V_values, "time": times},
-        allow_pickle=True,
-    )
 
-
-# Finally we solve it
+# Finally we solve it. Here we also compute the average voltage inside the S2 domain, and if time is lager than 100 ms and the voltage drops below - 70 mV we will initiate the S2 stimulus
 
 t = 0.0
 save_freq = int(1.0 / dt)
 i = 0
+s2_triggered = False
 while t < end_time + 1e-12:
     # Make sure to save at the same time steps that is used by Ambit
 
     if i % save_freq == 0:
         save(t)
 
+        # Check if average voltage in S2 domain is below threshold
+        v_s2_avg = dolfin.assemble(solver.pde.state * I_s_s2.dz) / vol_s2_domain
+        print(f"Average voltage in S2 domain = {v_s2_avg}")
+
+        if not s2_triggered and (t > 100.0 and v_s2_avg < -70.0):
+            print("Initiating S2 stimulation")
+            s2_start.assign(t)
+            s2_triggered = True
+
+
     solver.step((t, t + dt))
     i += 1
     t += dt
-
 
 # ```{figure} ../docs/_static/reentry.gif
 # ---
 # name: reentry
 # ---
 # ```
-
 # ```{bibliography}
 # ```
 #
